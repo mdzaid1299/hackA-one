086b0b8f41dec46e52d1cef653cff00c847c1883
Nilufa Khan <khannilufa5@gmail.com>
2023-02-25 23:09:14 +0530

    q1

---
+++ wife/Question1.java
@@ -0,0 +1,8 @@
+package wife;
+//withdrawl 0.50%
+//deposit 0.25% amount charged as processing fee
+// if two successive transactions are of different type
+// the charges are 0.40% and 0.20% on withdrawal and deposit respectively on a current transaction.
+
+public class Question1 {
+}
